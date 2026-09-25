d069040ad562b827b0b520576185932439f04658
navalsson <navalanthony.a@gmailcom>
2026-09-25 14:15:49 -0500

    Agrega base de Spring Boot y Thymeleaf

---
+++ src/main/java/com/PatitasFelices/SistemaDeVeterinario/SistemaDeVeterinarioApplication.java
@@ -0,0 +1,13 @@
+package com.PatitasFelices.SistemaDeVeterinario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVeterinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeVeterinarioApplication.class, args);
+	}
+
+}
